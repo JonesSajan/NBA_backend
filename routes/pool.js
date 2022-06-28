79783dbfb423956106f5f0a949e41e9019083646
Jones <jonessajan3000@gmail.com>
2022-06-27 20:27:19 -0700

    modification for deployment

--- routes/pool.js
+++ routes/pool.js
@@ -1,13 +1,13 @@
 let mysql = require("mysql");
 let pool = mysql.createPool({
-    host: 'localhost',
-    port: 3306,
-    user: 'root',
-    password: 'password',
-    database: 'nba',
-    connectionLimit: 100,
-    insecureAuth: true,
-    multipleStatements: true
-})
+  host: "sql5.freesqldatabase.com",
+  port: 3306,
+  user: "sql5502477",
+  password: "hCrcIIlGLt",
+  database: "sql5502477",
+  connectionLimit: 100,
+  insecureAuth: true,
+  multipleStatements: true,
+});
 
-module.exports = pool
+module.exports = pool;
