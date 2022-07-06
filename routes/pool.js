0aa0ff1cc35fda9384ecdfe6249aa2f9d39e2d60
JonesSajan <92352695+JonesSajan@users.noreply.github.com>
2022-07-06 10:49:58 -0700

    remote database changed

--- routes/pool.js
+++ routes/pool.js
@@ -1,13 +1,13 @@
 let mysql = require("mysql");
 let pool = mysql.createPool({
-    host: 'localhost',
-    port: 3306,
-    user: 'root',
-    password: 'password',
-    database: 'nba',
-    connectionLimit: 100,
-    insecureAuth: true,
-    multipleStatements: true
-})
+  host: "remotemysql.com",
+  port: 3306,
+  user: "qrLbOiQxfS",
+  password: "zSV8aphHyi",
+  database: "qrLbOiQxfS",
+  connectionLimit: 100,
+  insecureAuth: true,
+  multipleStatements: true,
+});
 
-module.exports = pool
+module.exports = pool;
